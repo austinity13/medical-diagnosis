63cd1308ebd9f45e5fc9fa2eb88afa5838af59f7
austinity13 <113301815+austinity13@users.noreply.github.com>
2026-03-24 13:07:14 -0500

    Update cases.js

--- cases.js
+++ cases.js
@@ -46,4 +46,51 @@ const CASES = [
       explanation: "Infection confirmed via imaging."
     }
   }
+   {
+    id: 1,
+    name: "Coughting Fit",
+    difficulty: "Medium",
+    vitals: { HR: 110, BP: "100/65", O2: 88, Temp: 98.6 },
+    stages: [
+      {
+        type: "info",
+        title: "History",
+        text: "27-year-old with cough, chest pain, shortness of breath, history of smoking"
+      },
+      {
+        type: "choice",
+        title: "Next Step",
+        options: [
+          { text: "Order chest X-ray", correct: true, next: 2, effect: { O2: 2 } },
+          { text: "Send home", correct: false, feedback: "Patient worsens at home.", effect: { O2: -5, HR: 10 }, fail: true }
+        ]
+      },
+      {
+        type: "info",
+        title: "Results",
+        text: "X-ray shows mucus buildup."
+      },
+      {
+        type: "choice",
+        title: "Diagnosis",
+        options: [
+          { text: "Cold", correct: false },
+          { text: "Pneumonia", correct: true },
+          { text: "Bronchitis", correct: false }
+        ]
+      },
+      {
+        type: "choice",
+        title: "Treatment",
+        options: [
+          { text: "Water and Humidifier", correct: true, effect: { Temp: -2, HR: -10 } },
+          { text: "Ignore", correct: false, effect: { O2: -10 }, fail: true }
+        ]
+      }
+    ],
+    final: {
+      diagnosis: "Bronchitis",
+      explanation: "Infection confirmed via imaging."
+    }
+  }
 ];
